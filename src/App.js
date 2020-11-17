66abbad9b7528f68f5c34688ab83f2afd2ce5bac
Paula Suarez <Paula.Suarez@mainevent.com>
2020-11-17 16:44:18 -0600

    Adding recent updates

--- src/App.js
+++ src/App.js
@@ -9,33 +9,56 @@ import styled from 'styled-components';
 function App() {
   return (
 
-    <Layout> 
-    <div className="App">
+    <Layout>
+      <div className="App">
 
 
-      {/* <Animated animationIn="lightSpeedIn" animationOut="zoomOutDown" animationInDuration={4000} animationOutDuration={1000} isVisible={true}>
+        {/* <Animated animationIn="lightSpeedIn" animationOut="zoomOutDown" animationInDuration={4000} animationOutDuration={1000} isVisible={true}>
         <img className="img-responsive" src="https://cdn1.vectorstock.com/i/thumb-large/63/85/black-smiley-flat-silhouette-white-background-vector-26856385.jpg" alt="pawel" width="50%" />
 
       </Animated> */}
 
-      <h2>Fade in Overlay</h2>
-      <p>Hover over the image to see the effect.</p>
 
-      <div class="container">
-        <img src="https://cdn1.vectorstock.com/i/thumb-large/63/85/black-smiley-flat-silhouette-white-background-vector-26856385.jpg" alt="pawel" width="50%" />
-          <div class="overlay">
-            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <h2>About Us</h2>
+        <p>Meet Pawel, Nicole and Paula</p>
+
+        <div class="container">
+          <div className="row">
+          <img src="https://cdn1.vectorstock.com/i/thumb-large/63/85/black-smiley-flat-silhouette-white-background-vector-26856385.jpg" alt="pawel" />
+          <div class="overlay1">
+            <div class="text">Pawel is lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
           </div>
+          </div>
+
+          <div className="row">
+          <img src="https://cdn1.vectorstock.com/i/thumb-large/63/85/black-smiley-flat-silhouette-white-background-vector-26856385.jpg" alt="pawel" />
+          <div class="overlay">
+            <div class="text">Pawel is lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
+          </div>
+
+
+
+
+
+
+
+
         </div>
+
+
+
+
 
       </div>
 
-</Layout>
-  
+
+    </Layout >
+
   );
 }
 
-const Layout = styled.div `
+const Layout = styled.div`
 
 .container {
   position: relative;
@@ -44,7 +67,7 @@ const Layout = styled.div `
 
 .image {
   display: block;
-  width: 100%;
+  width: 50%;
   height: auto;
 }
 
@@ -54,27 +77,51 @@ const Layout = styled.div `
   bottom: 0;
   left: 0;
   right: 0;
-  height: 100%;
-  width: 100%;
+  height: auto;
+  width: 50%;
   opacity: 0;
   transition: .5s ease;
-  background-color: #008CBA;
+  background-color: green;
 }
 
-.container:hover .overlay {
-  opacity: 10;
+
+
+.container:hover .overlay{
+  opacity: 1;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 .text {
+  // color: black;
+  // font-size: 12px;
+  // position: absolute;
+  // top: 50%;
+  // left: 50%;
+  // -webkit-transform: translate(-50%, -50%);
+  // -ms-transform: translate(-50%, -50%);
+  // transform: translate(-50%, -50%);
+  // text-align: justify;
+  
   color: white;
-  font-size: 12px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  text-align: center;
+  font-size: 16px;
+  text-align: justify;
+  margin: 5px;
+  padding-top" 2px;
+
+  
+  
 }
 
 
